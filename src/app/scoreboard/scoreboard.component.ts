fb3b8b7e4c6ab2122abccdd8ee8374e5e761a249
eduinaalicka <eduina.alicka@gmail.com>
2024-05-03 21:55:54 +0200

    fix

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -7,27 +7,17 @@ import { ValidWordsService } from '../valid-words.service';
   styleUrls: ['./scoreboard.component.scss']
 })
 export class ScoreboardComponent {
-  public scoreCalculate = this.validWordsServiceService.calculateScore();
-  public two: number = Math.floor(this.scoreCalculate * 0.02);
-  public five: number = Math.floor(this.scoreCalculate * 0.05);
-  public eight: number = Math.floor(this.scoreCalculate * 0.08);
-  public fifteen: number = Math.floor(this.scoreCalculate * 0.15);
-  public twentyfive: number = Math.floor(this.scoreCalculate * 0.25);
-  public forty: number = Math.floor(this.scoreCalculate * 0.40);
-  public fifty: number = Math.floor(this.scoreCalculate * 0.50);
-  public seventy: number = Math.floor(this.scoreCalculate * 0.70);
+  public scoreCalculate :number;
+  public two: number;
+  public five: number;
+  public eight: number;
+  public fifteen: number;
+  public twentyfive: number;
+  public forty: number;
+  public fifty: number;
+  public seventy: number;
 
-  public scoreLevels: { name: string, minScore: number }[] = [
-    { name: 'Vezë', minScore: 0 },
-    { name: 'Larvë', minScore: this.two },
-    { name: 'Bletë', minScore: this.five },
-    { name: 'Bletë punëtore', minScore: this.eight },
-    { name: 'Bletë e zgjuar', minScore: this.fifteen },
-    { name: 'Bletë mendjendritur', minScore: this.twentyfive },
-    { name: 'Të lumtë thumbi!', minScore: this.forty },
-    { name: 'Bletar', minScore: this.fifty },
-    { name: 'Mbretëreshë!', minScore: this.seventy }
-  ];
+  public scoreLevels: { name: string, minScore: number }[]
   public validWords: { word: string, isPangram: boolean }[] = [];
   public score: number = 0;
   public scoreName: string = '';
@@ -35,6 +25,28 @@ export class ScoreboardComponent {
   constructor(
     private validWordsServiceService: ValidWordsService
   ) {
+    this.scoreCalculate = this.validWordsServiceService.calculateScore();
+    this.two = Math.floor(this.scoreCalculate * 0.02);
+    this.five = Math.floor(this.scoreCalculate * 0.05);
+    this.eight = Math.floor(this.scoreCalculate * 0.08);
+    this.fifteen = Math.floor(this.scoreCalculate * 0.15);
+    this.twentyfive = Math.floor(this.scoreCalculate * 0.25);
+    this.forty = Math.floor(this.scoreCalculate * 0.40);
+    this.fifty = Math.floor(this.scoreCalculate * 0.50);
+    this.seventy = Math.floor(this.scoreCalculate * 0.70);
+
+    this.scoreLevels = [
+      { name: 'Vezë', minScore: 0 },
+      { name: 'Larvë', minScore: this.two },
+      { name: 'Bletë', minScore: this.five },
+      { name: 'Bletë punëtore', minScore: this.eight },
+      { name: 'Bletë e zgjuar', minScore: this.fifteen },
+      { name: 'Bletë mendjendritur', minScore: this.twentyfive },
+      { name: 'Të lumtë thumbi!', minScore: this.forty },
+      { name: 'Bletar', minScore: this.fifty },
+      { name: 'Mbretëreshë!', minScore: this.seventy }
+    ];
+
     //subscribe to the validWordsSubject
     this.validWordsServiceService.validWordsSubject.subscribe(words => {
       this.validWords = words;
