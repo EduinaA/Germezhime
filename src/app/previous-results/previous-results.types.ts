bf8e3cfbff13e5ffabb05f1a0d653ad0c3100861
eduinaalicka <eduina.alicka@gmail.com>
2024-05-12 23:59:21 +0200

    daily update

--- src/app/previous-results/previous-results.types.ts
+++ src/app/previous-results/previous-results.types.ts
@@ -1,10 +1,11 @@
 export const wordsList: string[] = [
-  "akull", "akulli", "akulluara", "aura", "ikur", "ikura", "irur", "kakrruk", "kakrruku", "kalarua", "kalkulua", "kalkuluar", "kalkuluari", "kallu", "kallur", "kallura", "kalua", "kaluaka", "kaluar", "kaluara",
-  "kaull", "kaur", "kauri", "klikuar", "klikuari", "krua", "kruaka", "kruar", "kruara", "kuak", "kuaka", "kuaku", "kuakur", "kuku", "kukull", "kukulla", "kukuraq", "kulak", "kular", "kulari", "kull",
-  "kulla", "kullua", "kulluaka", "kulluar", "kulluara", "kulluari", "kulu", "kulur", "kuqa", "kuqal", "kuqalak", "kuqi", "kuqka", "kuqla", "kuqu", "kuqua", "kuqur", "kuqura", "kura", "kurala", "kurua",
-  "kuruaka", "kuruar", "kuruara", "laiku", "lakua", "lakuaka", "lakuar", "lakuara", "lakuriq", "lakuriqka", "lakuriqu", "lakuriqur", "larakuq", "laraquk", "larua", "laruari", "laruk", "laura", "lirua",
-  "liruaka", "liruar", "liruara", "liruari", "llaku", "llakuar", "llau", "lluka", "llulla", "llullaq", "lluq", "luari", "luaria", "luri", "qarku", "qarkuar", "qarkull", "qarkullua", "qarkulluar",
-  "qarkulluara", "qiku", "quaka", "quar", "quari", "quka", "qukali", "quku", "qull", "qulla", "qulli", "qullka", "qullu", "qullur", "qullura", "qulluri", "qurra", "qurrak", "rilau", "rrallua",
-  "rralluar", "rralluara", "rralluari", "rralluk", "rrua", "rruaka", "rruar", "rruara", "ruka", "ruku", "ruluar", "rural", "ulka", "ulli", "ulliri", "ulluk", "ullulla", "uluar", "uluari", "ulur",
-  "ulura", "uluri", "uria", "uriq", "urla", "urra", "urua", "uruaka", "uruar", "uruara"
+  "absolut", "alto", "auto", "autobot", "autobus", "babo", "ballo", "ballota",
+  "balo", "balos", "balto", "baltos", "baltosa", "baobab", "batallo", "bato",
+  "blato", "bobo", "bobol", "bobola", "bobolat", "bobota", "bobotat", "boll",
+  "bolla", "bota", "boto", "botua", "botuat", "bubullo", "bulo", "buto",
+  "labot", "laso", "lasos", "lasot", "lato", "llasto", "lolo", "lolos",
+  "lolot", "losa", "loto", "lotuat", "lulo", "obat", "obobo", "obus",
+  "saboto", "sabotua", "sallo", "sallos", "salto", "soba", "sobat", "soll",
+  "solla", "solo", "sosa", "sosu", "stol", "stola", "tablo", "tato", "total",
+  "tubo"
 ];
--- src/app/valid-words.service.ts
+++ src/app/valid-words.service.ts
@@ -10,7 +10,7 @@ export class ValidWordsService {
   private readonly validWordsStorageKey = 'validWords';
   private readonly currentLettersStorageKey = 'currentLetters';
   // Daily update of letters
-  public readonly letters: string[] = ['s', 'u', 'a', 'o', 't', 'b', 'l'];
+  public readonly letters: string[] = ['e', 'n', 'd', 'j', 'r', 'o', 'ç'];
 
   // The set of words that are available from the given letters
   public allValidWordsSet: Set<string> = new Set();
