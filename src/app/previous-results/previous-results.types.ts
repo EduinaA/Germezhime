0ac193599f0305b2a187ea85b15940c9f33cb0c2
eduinaalicka <eduina.alicka@gmail.com>
2024-05-13 23:58:24 +0200

    daily update

--- src/app/previous-results/previous-results.types.ts
+++ src/app/previous-results/previous-results.types.ts
@@ -1,11 +1,9 @@
 export const wordsList: string[] = [
-  "absolut", "alto", "auto", "autobot", "autobus", "babo", "ballo", "ballota",
-  "balo", "balos", "balto", "baltos", "baltosa", "baobab", "batallo", "bato",
-  "blato", "bobo", "bobol", "bobola", "bobolat", "bobota", "bobotat", "boll",
-  "bolla", "bota", "boto", "botua", "botuat", "bubullo", "bulo", "buto",
-  "labot", "laso", "lasos", "lasot", "lato", "llasto", "lolo", "lolos",
-  "lolot", "losa", "loto", "lotuat", "lulo", "obat", "obobo", "obus",
-  "saboto", "sabotua", "sallo", "sallos", "salto", "soba", "sobat", "soll",
-  "solla", "solo", "sosa", "sosu", "stol", "stola", "tablo", "tato", "total",
-  "tubo"
+  "deje", "dejçe", "dendej", "dendje", "denje", "denjo", "denjoj", "denjoje", "denjon", "derreje",
+  "djerr", "djerre", "djerroj", "djerron", "doje", "dorje", "drenje", "drenoj", "droje", "endej",
+  "endeje", "endje", "endjeje", "endjen", "eroj", "eroje", "errej", "errje", "jene", "jone",
+  "ndej", "ndeje", "ndenj", "ndenje", "ndenjeje", "ndenjen", "ndenjje", "ndenjjeje", "nderje", "nderjeje",
+  "nderoj", "nderoje", "ndje", "ndjej", "ndjeje", "ndjen", "ndjenje", "ndoreje", "nejç", "odeje",
+  "reje", "rendje", "rendoj", "roje", "roneje", "rrej", "rreje", "rroj", "rroje", "çendej",
+  "çendje", "çeçeje", "çeçeneje", "çeçoje", "çjerr", "çjerre", "çjerrje", "çnderoj", "çnderoje", "çoje"
 ];
