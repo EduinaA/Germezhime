bd4cdf27e1b753cad35eb968234e92a851952069
eduinaalicka <eduina.alicka@gmail.com>
2024-11-02 01:39:49 +0100

    daily update

--- src/app/valid-words.service.ts
+++ src/app/valid-words.service.ts
@@ -10,7 +10,7 @@ export class ValidWordsService {
   private readonly validWordsStorageKey = 'validWords';
   private readonly currentLettersStorageKey = 'currentLetters';
   // Daily update of letters
-  public readonly letters: string[] = ['j', 'h', 'v', 'z', 'a', 'e', 't'];
+  public readonly letters: string[] = ['m', 'e', 'y', 's', 'v', 'ë', 'h'];
 
   // The set of words that are available from the given letters
   public allValidWordsSet: Set<string> = new Set();
